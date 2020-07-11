3af1ba871b1fb38337d58835ed57011cdcf000fe
Jonathan Chang <ttjtftx@gmail.com>
2020-07-11 18:43:43 +0800

    Use fetch() to show greeting

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,8 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+function getGreetingUsingArrowFunctions() {
+  fetch('/data').then(response => response.text()).then((quote) => {
+    document.getElementById('greeting-container').innerText = quote;
+  });
+}
